1ad5515daf5360a88fbd57951c67bf34ca03ba8e
Fil31 <lolid76@gmail.com>
2022-03-12 12:57:51 +0300

    test git

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string userFirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string userLastName = Console.ReadLine();
+            Console.WriteLine($"Privet, {userFirstName} {userLastName}!"); //string interpolation
+            //wdConsole.WriteLine("Привет," + userFirstName + " " + userLastName + "!"); //string concantention 
+        }
+    }
+}
